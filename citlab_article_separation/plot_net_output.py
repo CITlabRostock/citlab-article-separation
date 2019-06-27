9c7c414f543b3610f6566fffb1ef2334c18ad9a0
Max Weidemann <max.weidemann499@gmail.com>
2019-06-27 14:07:55 +0200

    Plot net output -> tensorflow added to requirement file.

--- citlab_article_separation/plot_net_output.py
+++ citlab_article_separation/plot_net_output.py
@@ -1,14 +1,16 @@
 from __future__ import print_function, division
 
-import time
+import os
+from argparse import ArgumentParser
 
+import cv2
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import numpy as np
 
 
 def load_graph(frozen_graph_filename):
-    # We load the protobuf file from the disk and parse it to retrieve the
-    # unserialized graph_def
+    # We load the protobuf file from the disk and parse it to retrieve the unserialized graph_def
     with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
@@ -27,67 +29,71 @@ def load_graph(frozen_graph_filename):
     return graph
 
 
-class Validator_pb(object):
-    """
-        Validate a arunet instance
-        :param net: the arunet instance to train
-        """
+def plot_net_output(path_to_pb, path_to_img_lst, gpu_device="0"):
+    session_conf = tf.ConfigProto()
+    session_conf.gpu_options.visible_device_list = gpu_device
 
-    def __init__(self, path_to_pb, n_class, cost_kwargs={}):
-        self.graph = load_graph(path_to_pb)
-        self.cost_kwargs = cost_kwargs
-        self.n_class = n_class
+    graph = load_graph(path_to_pb)
 
-    def validate(self, data_provider, print_result=True, gpu_device="0"):
-        val_size = data_provider.size_val
-        session_conf = tf.ConfigProto()
-        session_conf.gpu_options.visible_device_list = gpu_device
-        with tf.Session(graph=self.graph, config=session_conf) as sess:
-            x = self.graph.get_tensor_by_name('inImg:0')
-            log = self.graph.get_tensor_by_name('logits:0')
-            predictor = self.graph.get_tensor_by_name('output:0')
-            tgt = tf.placeholder("float", shape=[None, None, None, self.n_class])
-            cost = get_cost(log, tgt, self.cost_kwargs)
-            print("Start validation")
+    with tf.Session(graph=graph, config=session_conf) as sess:
+        x = graph.get_tensor_by_name('inImg:0')
+        out = graph.get_tensor_by_name('output:0')
 
-            total_loss = 0
-            time_val_step = time.time()
-            for step in range(0, val_size):
-                batch_x, batch_tgt = data_provider.next_data('val')
-                if batch_x is None:
-                    print("No Validation Data available. Skip Validation Path.")
-                    break
-                # Run validation
-                loss, aPred = sess.run([cost, predictor],
-                                       feed_dict={x: batch_x, tgt: batch_tgt})
-                total_loss += loss
-                print("Act Loss: " + str(loss))
-                if print_result:
-                    n_class = aPred.shape[3]
-                    channels = batch_x.shape[3]
-                    fig = plt.figure()
-                    for aI in range(0, n_class + 1):
-                        if aI == 0:
-                            a = fig.add_subplot(1, n_class + 1, 1)
-                            if channels == 1:
-                                plt.imshow(batch_x[0, :, :, 0], cmap=plt.cm.gray)
-                            else:
-                                plt.imshow(batch_x[0, :, :, :])
-                            a.set_title('input')
-                        else:
-                            a = fig.add_subplot(1, n_class + 1, aI + 1)
-                            plt.imshow(aPred[0, :, :, aI - 1], cmap=plt.cm.gray, vmin=0.0, vmax=1.0)
-                            a.set_title('Channel: ' + str(aI - 1))
-                    plt.show()
-            total_loss = total_loss / val_size
-            time_used = time.time() - time_val_step
-            self.output_epoch_stats_val(total_loss, time_used)
+        with open(path_to_img_lst) as f:
+            for path_to_img in f.readlines():
+                path_to_img = path_to_img.rstrip()
 
-            data_provider.stop_all()
-            print("Validation Finished!")
+                dirname = os.path.dirname(path_to_img)
+                img_name = os.path.basename(path_to_img)
+                img_name, ext = os.path.splitext(img_name)
 
-            return None
+                path_to_gt_0 = os.path.join(dirname, "C3", img_name + "_GT0" + ext)
+                path_to_gt_1 = os.path.join(dirname, "C3", img_name + "_GT1" + ext)
+                path_to_gt_2 = os.path.join(dirname, "C3", img_name + "_GT2" + ext)
 
-    def output_epoch_stats_val(self, total_loss, time_used):
-        print(
-            "VAL: Average loss: {:.8f}, time used: {:.2f}".format(total_loss, time_used))
+                img = cv2.imread(path_to_img)
+                gt0_img = cv2.imread(path_to_gt_0)
+                gt1_img = cv2.imread(path_to_gt_1)
+                gt2_img = cv2.imread(path_to_gt_2)
+                img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                if len(img_gray.shape) == 2:
+                    img_gray = np.expand_dims(img_gray, axis=-1)
+                    img_gray = np.expand_dims(img_gray, axis=0)
+                out_np = sess.run(out, feed_dict={x: img_gray})
+                out_img = out_np
+
+                # out_img = np.array((out_img > 0.2), np.uint8)
+
+                n_class_img = out_img.shape[-1]
+                fig = plt.figure()
+                a = fig.add_subplot(2, n_class_img + 2, 1)
+                plt.imshow(img, cmap=plt.cm.gray)
+                # plt.imshow(out_img[0, :, :, :], cmap=plt.cm.gray)
+                a.set_title('input image')
+
+                fig.add_subplot(2, n_class_img + 2, n_class_img + 4)
+                plt.imshow(gt0_img)
+                fig.add_subplot(2, n_class_img + 2, n_class_img + 5)
+                plt.imshow(gt1_img)
+                fig.add_subplot(2, n_class_img + 2, n_class_img + 6)
+                plt.imshow(gt2_img)
+
+                for cl in range(n_class_img):
+                    a = fig.add_subplot(2, n_class_img + 2, cl + 2)
+                    plt.imshow(out_img[0, :, :, cl], cmap=plt.cm.gray, vmin=0.0, vmax=1.0)
+                    cv2.imwrite("./tests/resources/test_plot_net_output/tmp/" + img_name + "_GT" + str(cl) + ext,
+                                out_img[0, :, :, cl] * 255)
+                    a.set_title('Channel: ' + str(cl))
+                plt.show()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--path_to_tf_graph', default='', type=str,
+                        help="path to the TensorFlow .pb file containing the ARU-Net graph.")
+    parser.add_argument('--path_to_img_lst', default='', type=str,
+                        help='path to the lst file containing the file paths of the images.')
+
+    args = parser.parse_args()
+
+    plot_net_output(args.path_to_tf_graph, args.path_to_img_lst)
