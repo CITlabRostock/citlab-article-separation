36d85a898a54bf75c80eeed955761c01e7ddd28f
Max Weidemann <max.weidemann499@gmail.com>
2019-11-19 14:54:27 +0100

    Plot net output together with input image + minor changes

--- citlab_article_separation/net_post_processing/textblock_net_post_processor.py
+++ citlab_article_separation/net_post_processing/textblock_net_post_processor.py
@@ -57,11 +57,11 @@ class TextBlockNetPostProcessor(object):
     def get_separators(self, threshold_horizontal=0.1, threshold_vertical=0.5):
 
         height, width = self.images['text_block'].shape
-        print(height, width)
 
         horizontal_profiles = np.sum(self.images['text_block'], axis=1) / 255
         vertical_profiles = np.sum(self.images['text_block'], axis=0) / 255
 
+        # We check for '<', because we search for blackruns in the textblock netoutput!
         horizontal_separators = [(i, hp / width) for i, hp in enumerate(horizontal_profiles) if
                                  hp / width < threshold_horizontal]
         vertical_separators = [(i, vp / height) for i, vp in enumerate(vertical_profiles) if
@@ -103,9 +103,9 @@ if __name__ == '__main__':
     white_sep = np.zeros(orig_image.shape, dtype=np.uint8)
     white_sep[:, index_vertical] = 255
     white_sep[index_horizontal, :] = 255
-    white_sep = cv2.resize(white_sep, None, fx=0.5, fy=0.5)
+    # white_sep = cv2.resize(white_sep, None, fx=0.5, fy=0.5)
 
-    separator_image = cv2.resize(separator_image, None, fx=0.5, fy=0.5)
+    # separator_image = cv2.resize(separator_image, None, fx=0.5, fy=0.5)
     separator_image = np.array((separator_image > 0.2), np.uint8)
     print(separator_image, separator_image.dtype)
     separator_image *= 255
@@ -126,15 +126,47 @@ if __name__ == '__main__':
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    # print(tb_pp.get_rotation_angle(tb_pp.original_image))
-    # print(tb_pp.get_rotation_angle(tb_outline_image))
-    # print(tb_pp.get_rotation_angle(tb_image))
-    # print(tb_pp.get_rotation_angle(separator_image))
+    vertical_profile = np.sum(black_white_separator, axis=0)
+    horizontal_profile = np.sum(black_white_separator, axis=1)
 
-    # a = np.array([[255, 0, 0, 0], [0, 255, 0, 0], [0, 0, 255, 0], [0, 0, 0, 255]])
-    # b = np.array([[255, 0, 0, 0], [255, 0, 0, 0], [255, 0, 0, 0], [255, 0, 0, 0]])
-    # # b = np.array([[1, 2, 3], [4, 5, 6]])
-    # # c = np.array([[0.1, 0.2], [1.2, 1.3]])
-    #
-    # _, best_angle = TextBlockNetPostProcessor.get_rotation_angle(b, delta=0.1, limit=90)
-    # print(round(best_angle, 4))
+    horizontal = [(i, hp / orig_image.shape[1] / 255) for i, hp in enumerate(horizontal_profile) if
+                  hp / orig_image.shape[1] / 255 < 0.2]
+    vertical = [(i, vp / orig_image.shape[0] / 255) for i, vp in enumerate(vertical_profile) if
+                vp / orig_image.shape[0] / 255 < 0.2]
+
+    horizontal_index = [i for i, _ in horizontal]
+    vertical_index = [i for i, _ in vertical]
+
+    print(horizontal_index)
+    print(vertical_index)
+
+
+    def convert_to_ranges(index_list):
+        range_list = []
+        skip = False
+        for i in range(len(index_list) - 1):
+            if not skip:
+                begin = index_list[i]
+            if index_list[i + 1] - index_list[i] < 3:
+                skip = True
+                continue
+            skip = False
+            end = index_list[i]
+            range_list.append((begin, end))
+        return range_list
+
+    print(convert_to_ranges(horizontal_index))
+    print(convert_to_ranges(vertical_index))
+
+    # tb_image_binarized = np.array((tb_image > 0.8), np.uint8) * 255
+    # print(tb_image_binarized)
+    # # erosion_kernel = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]], dtype=np.uint8)
+    # erosion_kernel = np.ones([8, 8], dtype=np.uint8)
+    # print(erosion_kernel)
+    # tb_image_erosion = cv2.erode(tb_image_binarized, erosion_kernel, iterations=1)
+    # tb_image_erosion = cv2.resize(tb_image_erosion, None, fx=0.4, fy=0.4)
+    # print(tb_image_erosion)
+    # cv2.imshow("erosion image textblock", tb_image_erosion)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # exit(1)
--- citlab_article_separation/plot_net_output.py
+++ citlab_article_separation/plot_net_output.py
@@ -4,6 +4,7 @@ import os
 from argparse import ArgumentParser
 
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
@@ -28,8 +29,25 @@ def load_graph(frozen_graph_filename):
     return graph
 
 
-def plot_net_output(path_to_pb, path_to_img_lst, save_folder="", gpu_device="0", rescale=None, mask_threshold=None,
-                    plot_with_gt=False):
+def plot_image_with_net_output(image, net_output):
+    net_output_rgb_int = np.uint8(cv2.cvtColor(net_output, cv2.COLOR_GRAY2BGR))
+    net_output_rgb_int = cv2.cvtColor(net_output_rgb_int, cv2.COLOR_BGR2HLS)
+
+    res = cv2.addWeighted(image, 0.9, net_output_rgb_int, 0.4, 0)
+    res = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
+    return res
+
+
+def plot_connected_components(image):
+    _, image_bin = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(255 - image_bin)
+
+    # res = cv2.addWeighted(image, 0.4, labels, 0.1, 0, dtype=2)
+    return labels
+
+
+def plot_net_output(path_to_pb, path_to_img_lst, save_folder="", gpu_device="0", rescale=None, fixed_height=None,
+                    mask_threshold=None, plot_with_gt=False, plot_with_img=False, show_plot=False):
     session_conf = tf.ConfigProto()
     session_conf.gpu_options.visible_device_list = gpu_device
 
@@ -47,10 +65,19 @@ def plot_net_output(path_to_pb, path_to_img_lst, save_folder="", gpu_device="0",
                 img_name, ext = os.path.splitext(os.path.basename(path_to_img))
 
                 img = cv2.imread(path_to_img)
+                img_height, img_width = img.shape[:2]
                 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-                if rescale:
-                    img_gray = cv2.resize(img_gray, None, fx=rescale, fy=rescale, interpolation=cv2.INTER_AREA)
+                scaling_factor = None
+                if fixed_height and rescale and rescale != 1:
+                    scaling_factor = rescale * fixed_height / img_height
+                elif fixed_height:
+                    scaling_factor = fixed_height / img_height
+
+                if scaling_factor:
+                    img = cv2.resize(img, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+                    img_gray = cv2.resize(img_gray, None, fx=scaling_factor, fy=scaling_factor,
+                                          interpolation=cv2.INTER_AREA)
 
                 if len(img_gray.shape) == 2:
                     img_gray = np.expand_dims(img_gray, axis=-1)
@@ -63,22 +90,45 @@ def plot_net_output(path_to_pb, path_to_img_lst, save_folder="", gpu_device="0",
                 n_class_img = out_img.shape[-1]
 
                 if plot_with_gt:
-                    paths_to_gts = [os.path.join(dirname, "C" + str(n_class_img), img_name + "_GT" + str(i) + ".png") for i in range(n_class_img)]
+                    paths_to_gts = [os.path.join(dirname, "C" + str(n_class_img), img_name + "_GT" + str(i) + ".png")
+                                    for i in range(n_class_img)]
                     gt_imgs = [cv2.imread(path_to_gt) for path_to_gt in paths_to_gts]
                     gt_imgs = [cv2.cvtColor(gt_img, cv2.COLOR_BGR2GRAY) for gt_img in gt_imgs]
-                    if rescale:
-                        gt_imgs = [cv2.resize(gt_img, None, fx=rescale, fy=rescale) for gt_img in gt_imgs]
+                    if scaling_factor:
+                        gt_imgs = [cv2.resize(gt_img, None, fx=scaling_factor, fy=scaling_factor) for gt_img in gt_imgs]
 
                 for cl in range(n_class_img):
                     out_img_2d = out_img[0, :, :, cl]
+                    out_img_2d_255 = out_img_2d * 255
+                    out_img_2d_255 = np.uint8(out_img_2d_255)
 
+                    if plot_with_img:
+                        out_img_2d_255 = plot_image_with_net_output(img, out_img_2d_255)
                     if plot_with_gt:
                         gt_img = gt_imgs[cl]
-                        out_img_2d = np.concatenate((out_img_2d, gt_img), axis=1)
-
-                    cv2.imwrite(
-                        os.path.join(save_folder, img_name + "_OUT" + str(cl) + ext), out_img_2d * 255)
-
+                        if plot_with_img:
+                            gt_img = plot_image_with_net_output(img, gt_img)
+                        out_img_2d_255 = np.concatenate((out_img_2d_255, gt_img), axis=1)
+                    if save_folder:
+                        cv2.imwrite(
+                            os.path.join(save_folder, img_name + "_OUT" + str(cl) + ext), out_img_2d_255)
+                    if show_plot:
+                        if plot_with_img:
+                            plt.imshow(out_img_2d_255)
+                        else:
+                            plt.imshow(out_img_2d_255, cmap="gray")
+                        img_gray_2d = img_gray[0, :, :, 0]
+                        #
+                        # img_with_net_output = plot_image_with_net_output(img, out_img_2d_255)
+                        # plt.imshow(img_with_net_output, cmap='gray')
+                        # plt.imshow(img)
+                        # print(img.shape)
+                        # plt.show()
+                        # plt.imshow(img_with_net_output)
+                        img_with_ccs = plot_connected_components(img_gray_2d)
+                        plt.imshow(img_with_ccs, cmap='gray')
+                        plt.show()
+                        exit(1)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -89,6 +139,8 @@ if __name__ == '__main__':
     parser.add_argument('--save_folder', default='', type=str, help="path to the save folder")
     parser.add_argument('--rescale_factor', default=1.0, type=float,
                         help="rescaling the images before inputting them to the network.")
+    parser.add_argument('--fixed_height', default=0, type=int,
+                        help="rescales the input images to a fixed height. Only works if rescale_factor is not set")
 
     args = parser.parse_args()
 
@@ -96,4 +148,5 @@ if __name__ == '__main__':
         os.mkdir(args.save_folder)
 
     plot_net_output(args.path_to_tf_graph, args.path_to_img_lst, args.save_folder, rescale=args.rescale_factor,
-                    plot_with_gt=False, mask_threshold=False)
+                    fixed_height=args.fixed_height, plot_with_gt=False, plot_with_img=True,
+                    mask_threshold=False, show_plot=False)
