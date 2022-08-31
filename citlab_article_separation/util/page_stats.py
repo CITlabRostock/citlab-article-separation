a8f45a6e3b1b42be4b3aa29a856f17b8b15062c6
Johannes Michael <johannesmichael1993@gmail.com>
2022-08-31 09:10:46 +0200

    added option to export page_stats to csv

--- citlab_article_separation/util/page_stats.py
+++ citlab_article_separation/util/page_stats.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import pandas
 from citlab_python_util.parser.xml.page.page import Page
 import citlab_python_util.parser.xml.page.page_constants as page_constants
 from citlab_python_util.logging.custom_logging import setup_custom_logger
@@ -26,14 +28,24 @@ def get_page_stats(path_to_pagexml, region_stats=True, text_line_stats=True, art
     :param path_to_pagexml: file path of the Page xml
     :return: list of polygons, list of article ids, image resolution
     """
+    res = dict()
+
     # load the page xml file
     logger.info(f"Processing {path_to_pagexml}")
     page_file = Page(path_to_pagexml)
     width, height = page_file.get_image_resolution()
     logger.info(f"- Image resolution: width={width}, height={height}")
+    res["ID"] = os.path.splitext(os.path.basename(path_to_pagexml))[0]
+    res["ImageWidth"] = width
+    res["ImageHeight"] = height
 
     # get regions
     dict_of_regions = page_file.get_regions()
+    if article_stats:
+        article_dict = page_file.get_article_dict()
+        logger.info(f"- Number of articles: {len(set(article_dict.keys()))}")
+        res["Articles"] = len(set(article_dict.keys()))
+
     if region_stats:
         for key in dict_of_regions:
             regions = dict_of_regions[key]
@@ -42,17 +54,18 @@ def get_page_stats(path_to_pagexml, region_stats=True, text_line_stats=True, art
                 for text_region in regions:
                     text_lines.extend(text_region.text_lines)
                 logger.info(f"- Number of {key}: {len(regions)}, number of text_lines: {len(text_lines)}")
+                res[key] = len(regions)
+                res["TextLine"] = len(text_lines)
             else:
                 logger.info(f"- Number of {key}: {len(dict_of_regions[key])}")
-
-    if article_stats:
-        article_dict = page_file.get_article_dict()
-        logger.info(f"- Number of articles: {len(set(article_dict.keys()))}")
+                res[key] = len(dict_of_regions[key])
+    return res
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pagexml_list', help="Input list with paths to pagexml files", required=True)
+    parser.add_argument('--csv_path', default="", help="Optionally save data in csv given by this path")
     parser.add_argument('--region_stats', type=bool, default=True, metavar="BOOL",
                         help="Get region stats or not.")
     parser.add_argument('--text_line_stats', type=bool, default=True, metavar="BOOL",
@@ -62,5 +75,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with open(args.pagexml_list, "r") as list_file:
+        page_data = []
         for path in list_file:
-            get_page_stats(path.rstrip())
+            stats = get_page_stats(path.rstrip(), args.region_stats, args.text_line_stats, args.article_stats)
+            page_data.append(list(stats.values()))
+        column_keys = list(stats.keys())
+
+    if args.csv_path:
+        df = pandas.DataFrame(page_data, columns=column_keys)
+        nan_columns = df.columns[df.isna().any()].tolist()  # check for columns containing NaN
+        if nan_columns:
+            nan_dict = dict(zip(nan_columns, ["int32"] * len(nan_columns)))
+            # replace NaN with 0 and cast columns to int (is float after fillna)
+            df = df.fillna(value=0).astype(nan_dict)
+            logger.info(f"Replaced NaN values with 0 in columns {nan_columns}")
+        df.to_csv(args.csv_path, index=False)
+        logger.info(f"Wrote data to {args.csv_path}")
